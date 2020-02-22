b2389391efde76b8f147d8f69863af9891bc131c
Thalyson Rodrigues <thalysonrodrigues.dev@gmail.com>
2020-02-22 10:56:04 -0400

    add middlewares and config session

--- app/src/index.js
+++ app/src/index.js
@@ -9,19 +9,38 @@ if (!process.env.NODE_ENV) {
 }
 
 import './config';
+import './database';
+
+const sess = {
+  key: process.env.SESSION_ID,
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    expires: 600000
+  }
+};
 
 import express from 'express';
 import routes from './routes';
 import bodyParser from 'body-parser';
 import expressLayouts from 'express-ejs-layouts';
 import path from 'path';
+import flash from 'express-flash-messages';
+import cookie from 'cookie-parser';
+import session from 'express-session';
+import override from 'method-override';
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'app', 'views'));
+app.use(session(sess));
+app.use(cookie());
+app.use(flash());
+app.use(override('_method'));
 app.use(expressLayouts);
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(routes);
 
