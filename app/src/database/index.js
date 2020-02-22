8e2b5e7083b46ea595fa842457c4e3d8a3c00a15
Thalyson Rodrigues <thalysonrodrigues.dev@gmail.com>
2020-02-22 16:51:23 -0400

    config connection to category and post

--- app/src/database/index.js
+++ app/src/database/index.js
@@ -2,9 +2,17 @@ import Sequelize from 'sequelize';
 import config from '../config/database';
 
 import User from '../app/models/User';
+import Post from '../app/models/Post';
+import Category from '../app/models/Category';
 
 const connection = new Sequelize(config[process.env.NODE_ENV]);
 
 User.init(connection);
+Post.init(connection);
+Category.init(connection);
+
+User.associate(connection.models);
+Post.associate(connection.models);
+Category.associate(connection.models);
 
 export default connection;
